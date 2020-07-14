6b34a133495bda4c0ed1a8a9336be13a7739f6fb
Jerud-Moyer <jerudmoyer@gmail.com>
2020-07-14 13:56:50 -0700

    fake data created

---
+++ data/cart.js
@@ -0,0 +1,15 @@
+const cart = [{
+    id: 'dragonslayer',
+    quantity: 3
+}, {
+    id: 'rockverb',
+    quantity: 1
+}, {
+    id: 'jcm800',
+    quantity: 2
+}, {
+    id: 'markv',
+    quntity: 2
+}];
+
+export default cart;
