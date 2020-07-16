37dce45b32e07c8b36dfd53a6e9b280bc93f0e46
Jerud-Moyer <jerudmoyer@gmail.com>
2020-07-16 13:00:46 -0700

    what?

--- products/products.js
+++ products/products.js
@@ -1,5 +1,5 @@
 import ampArr from '../data/ampCity.js';
-import { renderGuitarAmps } from './render-guitar-amps.js';
+import { renderGuitarAmps } from '../render-guitar-amps.js';
 const ampList = document.querySelector('#amps');
 
 for (let i = 0; i < ampArr.length; i ++) {
